15cfc9dc7de83bea37d0b1d5d116d2a688ead887
Mary-Kate James <marykatj@gmail.com>
2018-05-10 18:32:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,13 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holidays_supplies = [] 
-  holiday_hash.winter.values.flatten
+  holidays_supplies = []
+  season_contents = holiday_hash.values.flatten
+  if season_contents == :winter
+    holidays_supplies <<  season_contents.values.flatten
+  end
+  holidays_supplies
+     
 end
 
 def all_supplies_in_holidays(holiday_hash)

f858e8fad5b93b25ce113d5e94213bb5abca93da
Mary-Kate James <marykatj@gmail.com>
2018-05-10 18:28:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,6 +39,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holidays_supplies = [] 
   holiday_hash.winter.values.flatten
 end
 
